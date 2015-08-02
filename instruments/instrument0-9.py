751a502bcb6cb145fdb0f7b9b47e6fe77468c6da
Heinz <hz@gmail.com>
2015-08-02 19:33:49 +0200

    Misc fixes.

--- instruments/instrument0-9.py
+++ instruments/instrument0-9.py
@@ -1,3 +1,11 @@
+# Auther      : Heinz Samuelsson
+# Date        : 2015-08-02
+# File        : instrument0-9.py
+# Reference   : -
+# Description : Just loading one image of a dial.
+#
+# Python ver : 2.7.3 (gcc 4.6.3)
+
 import pygame, sys
 from pygame.locals import *
 import os
@@ -17,17 +25,15 @@ os.environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.init()
 screen = pygame.display.set_mode((WIDTH,HEIGHT), 0, 32)
 
-# load button images
-dial = pygame.image.load('instrument0-9.png')
+# load button images, if convert() is used then the background is affected
+dial = pygame.image.load('instrument0-9.png') #.convert()
 
-indexFont = pygame.font.SysFont("None",48)
-
-# fix background color to white
+# fix background
 background = pygame.Surface(screen.get_size())
 background.fill((0,100,100))
 screen.blit(background,(0, 0))
 
-# blit buttons on screen
+# blit dial on the screen
 screen.blit(dial,(DIAL_POS))
 
 pygame.display.flip()
@@ -38,6 +44,9 @@ while True:
 
    for event in pygame.event.get():
        if event.type == QUIT:
+           pygame.quit()
+           sys.exit()
+       elif event.type == KEYDOWN and event.key == pygame.K_ESCAPE:
            pygame.quit()
            sys.exit()
 
