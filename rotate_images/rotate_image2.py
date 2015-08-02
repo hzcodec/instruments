878a4ac167bea1b7a76465238d8713302cc758c0
Heinz <hz@gmail.com>
2015-08-02 18:28:21 +0200

    Comments added.

--- rotate_images/rotate_image2.py
+++ rotate_images/rotate_image2.py
@@ -17,14 +17,18 @@ RIGHT = 3
 
 # set rotation offset
 # if offset = 0 the rotation is around the middle of the image
-offsetX = 20
-offsetY = 20
+# the green/red rectangles will be equal
+offsetX = 30
+offsetY = 30
 
 pygame.init()
 screen = pygame.display.set_mode((640, 360), 0, 32)
 screen_rect = screen.get_rect()
 
 image = pygame.image.load('green_car.png')
+
+# if convert() is added then the green rectangle is partly hidden by the red rectangle
+#image = pygame.image.load('green_car.png').convert()
 
 imageRectangle = image.get_rect()
 center = imageRectangle.center
@@ -35,19 +39,14 @@ print 'Center:',center
 background = pygame.Surface(screen.get_size())
 
 background = background.convert()
-background.fill(WHITE)
 
 angle = 0
-mousePos = (0,0)
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-
-        elif event.type == pygame.MOUSEMOTION:
-            mousePos = pygame.mouse.get_pos()
 
         elif event.type == pygame.MOUSEBUTTONUP and event.button == RIGHT:
             angle += 10
@@ -72,24 +71,27 @@ while True:
     radAngle = math.radians(angle)
     xPos = math.cos(radAngle)
     yPos = math.sin(radAngle)
-#    print 'xPos:',xPos,'  -  yPos:',yPos
+    print 'xPos:',xPos*offsetX,'  -  yPos:',yPos*offsetY
 
     rotatedImageRectangle.center = (320,180)
+
+    # draw a rectangle before compensation of the angle
     pygame.draw.rect(screen, GREEN, (rotatedImageRectangle[0],
                                    rotatedImageRectangle[1],
                                    rotatedImageRectangle[2],
                                    rotatedImageRectangle[3]), 1)
-    print 'Green rectangle before update:',rotatedImageRectangle
+    #print 'Green rectangle before update:',rotatedImageRectangle
     
     # move rectangle according to the angle
     rotatedImageRectangle.center += np.array([np.cos(math.radians(angle)) * offsetX,
                                    -np.sin(math.radians(angle)) * offsetY])
-    print '  Red rectangle after update:',rotatedImageRectangle
 
+    #print '  Red rectangle after update:',rotatedImageRectangle
 
+    # blit rotated image
     screen.blit(rotatedImage, rotatedImageRectangle)
 
-    # draw a rectangle around the image
+    # draw a rectangle around the image after compensation of the angle
     pygame.draw.rect(screen, RED, (rotatedImageRectangle[0],
                                    rotatedImageRectangle[1],
                                    rotatedImageRectangle[2],
